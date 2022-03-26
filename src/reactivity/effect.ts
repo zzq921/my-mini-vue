9ea5394ec31929aed6f1e95ca878303d004eddab
zhaozhiqiang <zhaozhiqiang2@longfor.com>
2022-03-26 22:15:31 +0800

    优化stop

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,5 +1,8 @@
 
 import { extend } from "../shared"
+
+let activeEffect;
+let shouldTrack;
 class ReactiveEffect{
   private _fn:any
   deps = []
@@ -10,8 +13,14 @@ class ReactiveEffect{
    
   }
   run() {
+    if(!this.active) {
+      return this._fn()
+    }
+    shouldTrack = true
     activeEffect = this
-    return this._fn()
+    const result = this._fn();
+    shouldTrack = false
+    return result
   }
   stop() {
     if(this.active) {
@@ -28,11 +37,13 @@ function cleanupEffect(effect) {
   effect.deps.forEach((dep:any)=>{
     dep.delete(effect)
   })
+  effect.deps.length = 0
 }
 
 let targetMap = new Map()
 export function track(target,key) {
   //target key  dep
+  if(!isTracking())return
   let depsMap = targetMap.get(target)
   if(!depsMap) {
     depsMap = new Map()
@@ -43,9 +54,13 @@ export function track(target,key) {
     dep = new Set()
     depsMap.set(key,dep)
   }
-  if(!activeEffect)return
+  if(dep.has(activeEffect)) return
   dep.add(activeEffect)
   activeEffect.deps.push(dep)
+}
+
+function isTracking() {
+  return shouldTrack && activeEffect !==undefined
 }
 
 export function trigger(target,key) {
@@ -61,7 +76,7 @@ export function trigger(target,key) {
   }
 }
 
-let activeEffect;
+
 
 export function effect(fn,options:any = {}) {
  
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -68,9 +68,9 @@ describe("effect",()=>{
     obj.prop = 2
     expect(dummy).toBe(2)
     stop(runner)
-    obj.prop = 3
+    //obj.prop = 3
+    obj.prop++
     expect(dummy).toBe(2)
-
     runner()
     expect(dummy).toBe(3)
   })
