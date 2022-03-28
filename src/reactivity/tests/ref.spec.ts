63a637cd11135de16a2323520826c8d32c681fce
zhaozhiqiang <zhaozhiqiang2@longfor.com>
2022-03-28 22:22:33 +0800

    实现proxyRefs

--- src/reactivity/tests/ref.spec.ts
+++ src/reactivity/tests/ref.spec.ts
@@ -1,6 +1,6 @@
 import { effect } from "../effect"
 import { reactive } from "../reactive"
-import { isRef, ref, unRef } from "../ref"
+import { isRef, proxyRefs, ref, unRef } from "../ref"
 
 describe('ref',()=>{
   it('happy path',()=>{
@@ -48,9 +48,27 @@ describe('ref',()=>{
 
   it('unRef',()=>{
     const b = ref(1)
-   
     expect(unRef(b)).toBe(1)
-  
     expect(unRef(1)).toBe(1)
+  })
+
+  it('proxyRefs',()=>{
+    const user = {
+      age:ref(1),
+      b:2
+    }
+    const proxyUser = proxyRefs(user)
+    expect(user.age.value).toBe(1)
+    expect(proxyUser.age).toBe(1)
+    expect(proxyUser.b).toBe(2)
+
+    proxyUser.age = 20;
+    expect(proxyUser.age).toBe(20)
+    expect(user.age.value).toBe(20)
+
+    proxyUser.age = ref(10)
+    expect(proxyUser.age).toBe(10)
+    expect(user.age.value).toBe(10)
+   
   })
 })
