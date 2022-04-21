614b44a9dcd0dd50ce4cf6d849b23f20b7d2ad23
username <zhaozhiqiang2@longfor.com>
2022-04-21 22:15:17 +0800

    实现shapeFlags

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,5 +1,6 @@
 
 import { isObject } from "../shared/index"
+import { ShapeFlags } from "../shared/ShapeFlags"
 import { createComponentInstance, setupComponent } from "./component"
 
 export function render(vnode,container){
@@ -13,11 +14,11 @@ function path(vnode,container) {
   //如何区分是element或者component类型
   //processElement()
   //解构vnode，通过判断type的类型来区分是element或者component
-  const { type } = vnode
-  if(typeof type == "string") {
+  const { type,shapeFlag } = vnode
+  if(shapeFlag & ShapeFlags.ELEMENT) {
     //处理元素类型
     processElement(vnode,container)
-  }else if(isObject(type)) {
+  }else if(shapeFlag & ShapeFlags.STATEFUL_COMPONENT) {
     //处理组件类型
     processComponent(vnode,container)
   }
@@ -27,14 +28,14 @@ function processElement(vnode:any ,container:any) {
 }
 
 function mountElement(vnode:any,container:any) {
-  const {type,props,children} = vnode
+  const {type,props,children,shapeFlag} = vnode
   //如果是元素，则type就是元素本身，创建一个元素
   const el = (vnode.el = document.createElement(type));
   //元素的子节点children 有可能是string类型和array类型，也就是说子节点有可能是文本类型或者是一个数组。
-  if(typeof children === 'string') {
+  if(shapeFlag & ShapeFlags.TEXT_CHILDREN) {
     //如果是文本类型，直接赋值textContent
     el.textContent = children
-  }else if(Array.isArray(children)) {
+  }else if(shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
     //如果children是数组，循环遍历数组
     mountChildren(children,el)
   }
@@ -56,7 +57,7 @@ function mountChildren(children:any,container:any) {
 function processComponent(vnode:any ,container:any) {
   mountComponent(vnode,container)
 }
-
+//initinalVNode初始化节点
 function mountComponent(initinalVNode:any,container:any) { 
   const instance = createComponentInstance(initinalVNode)
   setupComponent(instance)
